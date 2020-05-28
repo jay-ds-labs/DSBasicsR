10de458f8a8ba40f898af9a400972777a76ebe6c
jay-ds-labs <jay.imt@gmail.com>
2020-05-28 11:51:35 +0000

    ml

--- Machine Learning.R
+++ Machine Learning.R
@@ -2024,6 +2024,334 @@ qplot(nodesize, acc)
 
 # UNDERSTANDING MACHINE LEARNING IN PRACTICE
 
+library(tidyverse)
+library(dslabs)
+mnist <- read_mnist()
+
+set.seed(1990, sample.kind = 'Rounding')
+index <- sample(nrow(mnist$train$images), 10000)
+x <- mnist$train$images[index,]
+y <- factor(mnist$train$labels[index])
+
+index <- sample(nrow(mnist$test$images), 1000)
+x_test <- mnist$test$images[index,]
+y_test <- factor(mnist$test$labels[index])
+
+colnames(x) <- 1:ncol(mnist$train$images)
+colnames(x_test) <- colnames(x)
+
+rm(mnist)
+
+library(matrixStats)
+sds <- colSds(x)
+qplot(sds, bins = 256)
+
+library(caret)
+nzv <- nearZeroVar(x)
+image(matrix(1:784 %in% nzv, 28, 28))
+col_index <- setdiff(1:ncol(x), nzv)
+length(col_index)
+
+
+# Labs
+models <- c("glm", "lda", "naive_bayes", "svmLinear", "knn", "gamLoess", "multinom", "qda", "rf", "adaboost")
+library(caret)
+library(dslabs)
+set.seed(1, sample.kind='Rounding')
+data("mnist_27")
+
+fits <- lapply(models, function(model){ 
+  print(model)
+  train(y ~ ., method = model, data = mnist_27$train)
+}) 
+
+names(fits) <- models
+fits[1]
+
+library(purrr)
+y_pred = map(c(1:10),function(i){
+                                      predict(fits[i],mnist_27$test)
+                                    })
+
+y_pred = as.data.frame(y_pred)
+y_pred$y = mnist_27$test$y
+
+y_accuracy = sapply(c(1:10), function(i){
+  mean(y_pred$y==y_pred[,i])
+})
+
+names(y_accuracy) = colnames(y_pred)[1:10]
+y_accuracy
+mean(y_accuracy)
+
+func_ensemble = function(r){
+  return(ifelse(sum(r==7)>5,7,2))
+}
+
+y_pred$y_ensemble= apply(y_pred[,-11],1,func_ensemble)
+
+y_ensemble_accuracy = mean(y_pred$y_ensemble == y_pred$y)
+sum(y_accuracy>y_ensemble_accuracy)
+y_accuracy[which(y_accuracy>y_ensemble_accuracy)]
+
+
+library(dslabs)
+data("tissue_gene_expression")
+dim(tissue_gene_expression$x)
+pca = prcomp(tissue_gene_expression$x)
+names(pca)
+dim(pca$x)
+pca$x[,c(1:2)]
+qplot(x=pca$x[,1],y=pca$x[,2], col=tissue_gene_expression$y)
+
+qplot(x=pca$x[,1],y=rowMeans(tissue_gene_expression$x), col=tissue_gene_expression$y)
+cor(pca$x[,1],rowMeans(tissue_gene_expression$x))
+
+pca_result = as.data.frame(pca$x)
+pca_result$y = tissue_gene_expression$y
+View(pca_result)
+pca_result %>% ggplot(aes(x=PC7, y = y)) + geom_boxplot()
+
+summary(pca)
+
+
+# MOVIE RECCO CASE STUDY
+library(tidyverse)
+library(dslabs)
+data("movielens")
+movielens %>% as_tibble()
+movielens %>% 
+  summarize(n_users = n_distinct(userId),
+            n_movies = n_distinct(movieId))
+
+671*9066
+nrow(movielens)
+
+library(caret)
+set.seed(755, sample.kind='Rounding')
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.2, 
+                                  list = FALSE)
+train_set <- movielens[-test_index,]
+test_set <- movielens[test_index,]
+test_set <- test_set %>% 
+  semi_join(train_set, by = "movieId") %>%
+  semi_join(train_set, by = "userId")
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+mu_hat <- mean(train_set$rating)
+mu_hat
+
+naive_rmse <- RMSE(test_set$rating, mu_hat)
+naive_rmse
+
+predictions <- rep(3, nrow(test_set))
+random_selection = RMSE(test_set$rating, predictions)
+random_selection
+
+rmse_results <- tibble(method = c("Just the average","Any random rating - 3"), RMSE = c(naive_rmse, random_selection))
+rmse_results
+
+mu <- mean(train_set$rating) 
+movie_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+qplot(b_i, data = movie_avgs, bins = 10, color = I("black"))
+
+predicted_ratings <- mu + test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  pull(b_i)
+RMSE(predicted_ratings, test_set$rating)
+
+train_set %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black")
+
+user_avgs <- train_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+predicted_ratings <- test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+RMSE(predicted_ratings, test_set$rating)
+
+test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  mutate(residual = rating - (mu + b_i)) %>%
+  arrange(desc(abs(residual))) %>%  
+  slice(1:10) %>% 
+  pull(title)
+
+movie_titles <- movielens %>% 
+  select(movieId, title) %>%
+  distinct()
+
+movie_avgs %>% left_join(movie_titles, by="movieId") %>%
+  arrange(desc(b_i)) %>% 
+  slice(1:10)  %>% 
+  pull(title)
+
+movie_avgs %>% left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  slice(1:10)  %>% 
+  pull(title)
+
+train_set %>% count(movieId) %>% 
+  left_join(movie_avgs, by="movieId") %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(desc(b_i)) %>% 
+  slice(1:10) %>% 
+  pull(n)
+
+train_set %>% count(movieId) %>% 
+  left_join(movie_avgs) %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  slice(1:10) %>% 
+  pull(n)
+
+lambda <- 3
+mu <- mean(train_set$rating)
+movie_reg_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i = n()) 
+
+tibble(original = movie_avgs$b_i, 
+       regularlized = movie_reg_avgs$b_i, 
+       n = movie_reg_avgs$n_i) %>%
+  ggplot(aes(original, regularlized, size=sqrt(n))) + 
+  geom_point(shape=1, alpha=0.5)
+
+train_set %>%
+  count(movieId) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(movie_titles, by = "movieId") %>%
+  arrange(desc(b_i)) %>% 
+  slice(1:10) %>% 
+  pull(title)
+
+train_set %>%
+  count(movieId) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10) %>% 
+  pull(title)
+
+predicted_ratings <- test_set %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  mutate(pred = mu + b_i) %>%
+  pull(pred)
+RMSE(predicted_ratings, test_set$rating)
+
+lambdas <- seq(0, 10, 0.25)
+
+mu <- mean(train_set$rating)
+just_the_sum <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(s = sum(rating - mu), n_i = n())
+
+rmses <- sapply(lambdas, function(l){
+  predicted_ratings <- test_set %>% 
+    left_join(just_the_sum, by='movieId') %>% 
+    mutate(b_i = s/(n_i+l)) %>%
+    mutate(pred = mu + b_i) %>%
+    pull(pred)
+  return(RMSE(predicted_ratings, test_set$rating))
+})
+qplot(lambdas, rmses)  
+lambdas[which.min(rmses)]
+
+
+
+
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu <- mean(train_set$rating)
+  
+  b_i <- train_set %>% 
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu)/(n()+l))
+  
+  b_u <- train_set %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u = sum(rating - b_i - mu)/(n()+l))
+  
+  predicted_ratings <- 
+    test_set %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    mutate(pred = mu + b_i + b_u) %>%
+    pull(pred)
+  
+  return(RMSE(predicted_ratings, test_set$rating))
+})
+
+qplot(lambdas, rmses)  
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+
+
+
+train_small <- movielens %>% 
+  group_by(movieId) %>%
+  filter(n() >= 50 | movieId == 3252) %>% ungroup() %>% 
+  group_by(userId) %>%
+  filter(n() >= 50) %>% ungroup()
+
+y <- train_small %>% 
+  select(userId, movieId, rating) %>%
+  spread(movieId, rating) %>%
+  as.matrix()
+
+
+rownames(y)<- y[,1]
+y <- y[,-1]
+
+movie_titles <- movielens %>% 
+  select(movieId, title) %>%
+  distinct()
+
+colnames(y) <- with(movie_titles, title[match(colnames(y), movieId)])
+
+
+y <- sweep(y, 2, colMeans(y, na.rm=TRUE))
+y <- sweep(y, 1, rowMeans(y, na.rm=TRUE))
+
+m_1 <- "Godfather, The"
+m_2 <- "Godfather: Part II, The"
+p1 <- qplot(y[ ,m_1], y[,m_2], xlab = m_1, ylab = m_2)
+
+m_1 <- "Godfather, The"
+m_3 <- "Goodfellas"
+p2 <- qplot(y[ ,m_1], y[,m_3], xlab = m_1, ylab = m_3)
+
+m_4 <- "You've Got Mail" 
+m_5 <- "Sleepless in Seattle" 
+p3 <- qplot(y[ ,m_4], y[,m_5], xlab = m_4, ylab = m_5)
+
+gridExtra::grid.arrange(p1, p2 ,p3, ncol = 3)
+
+x <- y[, c(m_1, m_2, m_3, m_4, m_5)]
+short_names <- c("Godfather", "Godfather2", "Goodfellas",
+                 "You've Got", "Sleepless")
+colnames(x) <- short_names
+r = cor(x, use="pairwise.complete")
+round(r, 1)
 
 
 
